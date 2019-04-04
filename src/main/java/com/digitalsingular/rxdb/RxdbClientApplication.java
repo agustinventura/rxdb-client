faf093520d13a561770ae682fadb394619ca2051
Agustín Ventura <agustinventura@gmail.com>
2019-04-04 17:07:25 +0200

    initial commit

---
+++ src/main/java/com/digitalsingular/rxdb/RxdbClientApplication.java
@@ -0,0 +1,13 @@
+package com.digitalsingular.rxdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RxdbClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RxdbClientApplication.class, args);
+	}
+
+}
