860206add6b46fa7f33e7abd5ae4f5028a32ff79
Agustín Ventura <agustinventura@gmail.com>
2019-04-04 18:43:52 +0200

    First web client test with blocking request.

--- src/main/java/com/digitalsingular/rxdb/RxdbClientApplication.java
+++ src/main/java/com/digitalsingular/rxdb/RxdbClientApplication.java
@@ -1,13 +1,21 @@
 package com.digitalsingular.rxdb;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.digitalsingular.rxdb.enclosure.Enclosure;
+import com.digitalsingular.rxdb.enclosure.EnclosureService;
 
-@SpringBootApplication
+import reactor.core.publisher.Flux;
+
 public class RxdbClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RxdbClientApplication.class, args);
+		EnclosureService enclosureService = new EnclosureService();
+		Flux<Enclosure> enclosures = enclosureService.getByPlot(1);
+		//enclosures.subscribe(RxdbClientApplication::handleEnclosure);
+		handleEnclosure(enclosures.blockLast());
+	}
+
+	private static void handleEnclosure(Enclosure enclosure) {
+		System.out.println("Enclosure " + enclosure.getId());
 	}
 
 }
