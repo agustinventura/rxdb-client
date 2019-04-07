a018d60ccb42ff37a2a5ba200d6bd17bc9d7bb47
Agustín Ventura <agustinventura@gmail.com>
2019-04-07 12:36:51 +0200

    Non rx client first commit

---
+++ src/main/java/com/digitalsingular/rxdb/enclosure/Enclosure.java
@@ -0,0 +1,327 @@
+package com.digitalsingular.rxdb.enclosure;
+
+public class Enclosure {
+
+	private int id;
+
+	private int province;
+
+	private int town;
+
+	private int plot;
+
+	private int code;
+
+	private String use;
+
+	private Integer aggregate;
+
+	private Integer zone;
+
+	private Float area;
+
+	private Float perimeter;
+
+	private Float sigpacArea;
+
+	private Float irrigationCoefficient;
+
+	private Float grazingCoefficient;
+
+	private Float averageSlope;
+
+	private Float maxSlope;
+
+	private Float minSlope;
+
+	private Float firstCategoryPercentage;
+
+	private Float secondCategoryPercentage;
+
+	private Float thirdCategoryPercentage;
+
+	private Float fourthCategoryPercentage;
+
+	private Float fifthCategoryPercentage;
+
+	private Float sixthCategoryPercentage;
+
+	private boolean dehesado;
+
+	private Float dehesadaAreaPercentage;
+
+	private Float dehesaPercentage;
+
+	private Float pasturesCoefficient;
+
+	private Float pasturesPercentage;
+
+	private Float xCoordinate;
+
+	private Float yCoordinate;
+
+	private boolean fifthUndeclared;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getProvince() {
+		return province;
+	}
+
+	public void setProvince(int province) {
+		this.province = province;
+	}
+
+	public int getTown() {
+		return town;
+	}
+
+	public void setTown(int town) {
+		this.town = town;
+	}
+
+	public int getPlot() {
+		return plot;
+	}
+
+	public void setPlot(int plot) {
+		this.plot = plot;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+	public void setCode(int code) {
+		this.code = code;
+	}
+
+	public String getUse() {
+		return use;
+	}
+
+	public void setUse(String use) {
+		this.use = use;
+	}
+
+	public Integer getAggregate() {
+		return aggregate;
+	}
+
+	public void setAggregate(Integer aggregate) {
+		this.aggregate = aggregate;
+	}
+
+	public Integer getZone() {
+		return zone;
+	}
+
+	public void setZone(Integer zone) {
+		this.zone = zone;
+	}
+
+	public Float getArea() {
+		return area;
+	}
+
+	public void setArea(Float area) {
+		this.area = area;
+	}
+
+	public Float getPerimeter() {
+		return perimeter;
+	}
+
+	public void setPerimeter(Float perimeter) {
+		this.perimeter = perimeter;
+	}
+
+	public Float getSigpacArea() {
+		return sigpacArea;
+	}
+
+	public void setSigpacArea(Float sigpacArea) {
+		this.sigpacArea = sigpacArea;
+	}
+
+	public Float getIrrigationCoefficient() {
+		return irrigationCoefficient;
+	}
+
+	public void setIrrigationCoefficient(Float irrigationCoefficient) {
+		this.irrigationCoefficient = irrigationCoefficient;
+	}
+
+	public Float getGrazingCoefficient() {
+		return grazingCoefficient;
+	}
+
+	public void setGrazingCoefficient(Float grazingCoefficient) {
+		this.grazingCoefficient = grazingCoefficient;
+	}
+
+	public Float getAverageSlope() {
+		return averageSlope;
+	}
+
+	public void setAverageSlope(Float averageSlope) {
+		this.averageSlope = averageSlope;
+	}
+
+	public Float getMaxSlope() {
+		return maxSlope;
+	}
+
+	public void setMaxSlope(Float maxSlope) {
+		this.maxSlope = maxSlope;
+	}
+
+	public Float getMinSlope() {
+		return minSlope;
+	}
+
+	public void setMinSlope(Float minSlope) {
+		this.minSlope = minSlope;
+	}
+
+	public Float getFirstCategoryPercentage() {
+		return firstCategoryPercentage;
+	}
+
+	public void setFirstCategoryPercentage(Float firstCategoryPercentage) {
+		this.firstCategoryPercentage = firstCategoryPercentage;
+	}
+
+	public Float getSecondCategoryPercentage() {
+		return secondCategoryPercentage;
+	}
+
+	public void setSecondCategoryPercentage(Float secondCategoryPercentage) {
+		this.secondCategoryPercentage = secondCategoryPercentage;
+	}
+
+	public Float getThirdCategoryPercentage() {
+		return thirdCategoryPercentage;
+	}
+
+	public void setThirdCategoryPercentage(Float thirdCategoryPercentage) {
+		this.thirdCategoryPercentage = thirdCategoryPercentage;
+	}
+
+	public Float getFourthCategoryPercentage() {
+		return fourthCategoryPercentage;
+	}
+
+	public void setFourthCategoryPercentage(Float fourthCategoryPercentage) {
+		this.fourthCategoryPercentage = fourthCategoryPercentage;
+	}
+
+	public Float getFifthCategoryPercentage() {
+		return fifthCategoryPercentage;
+	}
+
+	public void setFifthCategoryPercentage(Float fifthCategoryPercentage) {
+		this.fifthCategoryPercentage = fifthCategoryPercentage;
+	}
+
+	public Float getSixthCategoryPercentage() {
+		return sixthCategoryPercentage;
+	}
+
+	public void setSixthCategoryPercentage(Float sixthCategoryPercentage) {
+		this.sixthCategoryPercentage = sixthCategoryPercentage;
+	}
+
+	public boolean isDehesado() {
+		return dehesado;
+	}
+
+	public void setDehesado(boolean dehesado) {
+		this.dehesado = dehesado;
+	}
+
+	public Float getDehesadaAreaPercentage() {
+		return dehesadaAreaPercentage;
+	}
+
+	public void setDehesadaAreaPercentage(Float dehesadaAreaPercentage) {
+		this.dehesadaAreaPercentage = dehesadaAreaPercentage;
+	}
+
+	public Float getDehesaPercentage() {
+		return dehesaPercentage;
+	}
+
+	public void setDehesaPercentage(Float dehesaPercentage) {
+		this.dehesaPercentage = dehesaPercentage;
+	}
+
+	public Float getPasturesCoefficient() {
+		return pasturesCoefficient;
+	}
+
+	public void setPasturesCoefficient(Float pasturesCoefficient) {
+		this.pasturesCoefficient = pasturesCoefficient;
+	}
+
+	public Float getPasturesPercentage() {
+		return pasturesPercentage;
+	}
+
+	public void setPasturesPercentage(Float pasturesPercentage) {
+		this.pasturesPercentage = pasturesPercentage;
+	}
+
+	public Float getxCoordinate() {
+		return xCoordinate;
+	}
+
+	public void setxCoordinate(Float xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+
+	public Float getyCoordinate() {
+		return yCoordinate;
+	}
+
+	public void setyCoordinate(Float yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+
+	public boolean isFifthUndeclared() {
+		return fifthUndeclared;
+	}
+
+	public void setFifthUndeclared(boolean fifthUndeclared) {
+		this.fifthUndeclared = fifthUndeclared;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Enclosure other = (Enclosure) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+}
---
+++ src/main/java/com/digitalsingular/rxdb/enclosure/EnclosureService.java
@@ -0,0 +1,21 @@
+package com.digitalsingular.rxdb.enclosure;
+
+import java.util.Set;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+public class EnclosureService {
+
+	public Set<Enclosure> getByPlot(int plot) {
+		RestTemplate restTemplate = new RestTemplate();
+		ResponseEntity<Set<Enclosure>> response = restTemplate.exchange(
+		  "http://localhost:8080/enclosure?plot=1",
+		  HttpMethod.GET,
+		  null,
+		  new ParameterizedTypeReference<Set<Enclosure>>(){});
+		return response.getBody();
+	}
+}
